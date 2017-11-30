4c595be33549541d59d43ab8ca2c5d7c1e539e86
Jay Kapadnis <jayesh.kapadnis@gmail.com>
2017-11-30 13:41:02 +0530

    Added missing plugin configuration in child module

--- metadata-edit/src/main/scala/com/hashmap/haf/functions/FunctionsRegistry.scala
+++ metadata-edit/src/main/scala/com/hashmap/haf/functions/FunctionsRegistry.scala
@@ -13,6 +13,7 @@ class FunctionsRegistry {
 		//Have a local copy of those functions for faster lookup
 
 		registry += alias -> name
+		println("Check compile")
 		/*val f: Map[String, Predef.Function[_, _]] = registry.getOrElse((typeOf[I], typeOf[RT]), Map.empty)
 		registry += (typeOf[I], typeOf[RT]) -> (f + name -> func)*/
 	}
