a1b37618a9b4241970e7210368f26bc2d5775bfc
Jay Kapadnis <jayesh.kapadnis@gmail.com>
2017-12-07 17:33:12 +0530

    Adding external configuration support for spring boot project

--- functions-api/src/main/scala/com/hashmap/haf/functions/api/FunctionApiApplication.scala
+++ functions-api/src/main/scala/com/hashmap/haf/functions/api/FunctionApiApplication.scala
@@ -10,5 +10,16 @@ import org.springframework.context.annotation.ComponentScan
 class FunctionApiApplication
 
 object FunctionApiApplication extends App{
-	SpringApplication.run(classOf[FunctionApiApplication])
+	private val SPRING_CONFIG_NAME_KEY = "--spring.config.name"
+	private val DEFAULT_SPRING_CONFIG_PARAM = SPRING_CONFIG_NAME_KEY + "=" + "functions-api"
+
+	SpringApplication.run(classOf[FunctionApiApplication], updateArguments(args): _*)
+
+	private def updateArguments(args: Array[String]): List[String] ={
+		val argsAsList = args.toList
+		argsAsList.find(_.startsWith(SPRING_CONFIG_NAME_KEY)) match {
+			case Some(_) => argsAsList
+			case _ => DEFAULT_SPRING_CONFIG_PARAM :: argsAsList
+		}
+	}
 }
