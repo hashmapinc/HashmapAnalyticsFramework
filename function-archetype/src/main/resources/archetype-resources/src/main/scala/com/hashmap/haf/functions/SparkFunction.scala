aab29ed09a12a9deeb452885c10648aee19c0f42
Jay Kapadnis <jayesh.kapadnis@gmail.com>
2018-01-11 17:17:35 +0530

    Updated archetype with recent service API chanes

--- function-archetype/src/main/resources/archetype-resources/src/main/scala/com/hashmap/haf/functions/SparkFunction.scala
+++ function-archetype/src/main/resources/archetype-resources/src/main/scala/com/hashmap/haf/functions/SparkFunction.scala
@@ -1,8 +1,12 @@
 package com.hashmap.haf.functions
 
+import com.hashmap.haf.annotations.IgniteFunction
 import com.hashmap.haf.datastore.DataframeIgniteCache
+import com.hashmap.haf.functions.constants.TaskConfigurationConstants._
 import com.hashmap.haf.functions.services.ServiceFunction
-import org.apache.ignite.services.{Service, ServiceContext}
+import org.apache.ignite.Ignite
+import org.apache.ignite.resources.IgniteInstanceResource
+import org.apache.ignite.services.ServiceContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql
 import org.apache.spark.sql.types._
@@ -12,20 +16,19 @@ import org.apache.spark.sql.{Column, DataFrame, Row, SparkSession}
 class SparkFunction(configs: Map[String, String] = Map.empty[String, String]) extends ServiceFunction{
 
 	var appName = ""
-	val CONFIG = getClass.getResource("/cache.xml").getPath
-	override def run(inputKey: String, outputKey: String, config: Any): Unit = {
+	@IgniteInstanceResource
+	var ignite: Ignite = _
+
+	override def run(inputKey: String, outputKey: String, functionArguments: Map[String, String], configurations: Map[String, String]): String = {
 		val spark = SparkSession
 			.builder()
-			.appName("Sample service")
-			.master("local")
+			.appName(configurations.getOrElse(SPARK_APP_NAME, throw new IllegalArgumentException(SPARK_APP_NAME + " not provided in configurations")))
+			.master(configurations.getOrElse(SPARK_MASTER, throw new IllegalArgumentException(SPARK_MASTER + " not provided in configurations")))
 			.getOrCreate()
 
 		import spark.implicits._
-		val cache = DataframeIgniteCache.create(CONFIG)
+		val cache = DataframeIgniteCache.create()
 		val (schema, igniteRDD) = cache.get(spark.sparkContext, inputKey)
-
-		println("DF with following schema received: ")
-		schema.foreach(field => println(s"${field.name}: metadata=${field.metadata}"))
 
 		val rdd1: RDD[Row] = igniteRDD.map(_._2)
 		val df = spark.sqlContext.createDataFrame(rdd1, schema)
@@ -36,10 +39,9 @@ class SparkFunction(configs: Map[String, String] = Map.empty[String, String]) ex
 
 		cache.set(spark.sparkContext, newDs, outputKey)
 
-		println("DF with following schema has been saved: ")
-		newDs.schema.foreach(field => println(s"${field.name}: metadata=${field.metadata}"))
-
 		spark.close()
+
+		"Result"
 
 	}
 
