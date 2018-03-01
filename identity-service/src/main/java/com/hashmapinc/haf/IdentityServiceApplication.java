eb266e34dd4dff0784ac0b6138a60e43bc3691fb
Jay Kapadnis <jayesh.kapadnis@gmail.com>
2018-03-01 17:59:08 +0530

    Added Eureka suppor

--- identity-service/src/main/java/com/hashmapinc/haf/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/hashmapinc/haf/IdentityServiceApplication.java
@@ -3,10 +3,12 @@ package com.hashmapinc.haf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootConfiguration
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @EnableResourceServer
 @ComponentScan
