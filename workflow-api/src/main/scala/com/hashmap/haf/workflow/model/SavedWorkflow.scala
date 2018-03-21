e44d5ea8b1a82d77041074ec0dc38cc6f6d23858
Jetinder <jetinder.singhrathore@gamil.com>
2018-03-21 14:21:12 -0400

    Serializing the case classes

--- workflow-api/src/main/scala/com/hashmap/haf/workflow/model/SavedWorkflow.scala
+++ workflow-api/src/main/scala/com/hashmap/haf/workflow/model/SavedWorkflow.scala
@@ -6,7 +6,8 @@ import java.util
 case class SavedWorkflow(@BeanProperty id: String,
                          @BeanProperty name: String,
                          @BeanProperty xml: String,
-                         @BeanProperty tasks: util.List[WorkflowTask]){
+                         @BeanProperty tasks: util.List[WorkflowTask]) extends Serializable {
+  private val serialVersionUID = - 1262757341033966563L
   def this() = this("", "", "", null)
 }
 
--- workflow-api/src/main/scala/com/hashmap/haf/workflow/model/WorkflowTask.scala
+++ workflow-api/src/main/scala/com/hashmap/haf/workflow/model/WorkflowTask.scala
@@ -5,4 +5,6 @@ import scala.beans.BeanProperty
 case class WorkflowTask(@BeanProperty taskName: String,
                         @BeanProperty inputCache: String,
                         @BeanProperty outputCache: String,
-                        @BeanProperty toTaskName: String)
+                        @BeanProperty toTaskName: String) extends Serializable {
+  private val serialVersionUID = 6907745469068490450L
+}
