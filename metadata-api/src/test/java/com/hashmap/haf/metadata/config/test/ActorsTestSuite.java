32f35d3d006e288b8880094fad4288933424ee9d
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-06-19 20:07:15 +0530

    reverting changes in identity-service

--- metadata-api/src/test/java/com/hashmap/haf/metadata/config/test/ActorsTestSuite.java
+++
@@ -1,9 +0,0 @@
-package com.hashmap.haf.metadata.config.test;
-
-import org.junit.extensions.cpsuite.ClasspathSuite;
-import org.junit.runner.RunWith;
-
-@RunWith(ClasspathSuite.class)
-@ClasspathSuite.ClassnameFilters({"com.hashmap.haf.metadata.config.test.actors.*Test"})
-public class ActorsTestSuite {
-}
