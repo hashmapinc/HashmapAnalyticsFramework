a1f5b47d9efa47ecceb63e2d02986cff1e992df8
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-06-20 15:08:46 +0530

    Added response entity to RestController

--- metadata-api/src/main/java/com/hashmap/haf/metadata/config/controller/MetadataConfigController.java
+++ metadata-api/src/main/java/com/hashmap/haf/metadata/config/controller/MetadataConfigController.java
@@ -4,9 +4,11 @@ import com.hashmap.haf.metadata.config.exceptions.MetadataException;
 import com.hashmap.haf.metadata.config.model.MetadataConfig;
 import com.hashmap.haf.metadata.config.model.MetadataConfigId;
 import com.hashmap.haf.metadata.config.service.MetadataConfigService;
+import com.sun.org.apache.regexp.internal.RE;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,38 +26,56 @@ public class MetadataConfigController {
 
     @PreAuthorize("#oauth2.hasScope('server')")
     @RequestMapping(value = "/metaconfig", method = RequestMethod.POST)
-    public MetadataConfig saveMetadataConfig(@RequestBody MetadataConfig metadataConfig) {
+    public ResponseEntity saveMetadataConfig(@RequestBody MetadataConfig metadataConfig) {
         MetadataConfig savedMetadataConfig = checkNotNull(metadataConfigService.saveMetadataConfig(metadataConfig));
-        return savedMetadataConfig;
+        return ResponseEntity.status(HttpStatus.OK).body(savedMetadataConfig);
     }
 
     @PreAuthorize("#oauth2.hasScope('server')")
     @RequestMapping(value = "/metaconfig", method = RequestMethod.PUT)
-    public MetadataConfig updateMetadataConfig(@RequestBody MetadataConfig metadataConfig) {
+    public ResponseEntity updateMetadataConfig(@RequestBody MetadataConfig metadataConfig) {
         MetadataConfig savedMetadataConfig = checkNotNull(metadataConfigService.updateMetadataConfig(metadataConfig));
-        return savedMetadataConfig;
+        return ResponseEntity.status(HttpStatus.OK).body(savedMetadataConfig);
     }
 
     @PreAuthorize("#oauth2.hasScope('server')")
     @RequestMapping(value = "/metaconfig/{id}", method = RequestMethod.GET)
-    public MetadataConfig getMetadataConfig(@PathVariable String id){
+    public ResponseEntity getMetadataConfig(@PathVariable String id) {
         MetadataConfigId metadataConfigId =  new MetadataConfigId(UUID.fromString(id));
-        MetadataConfig metadataConfig = checkNotNull(metadataConfigService.findMetadataConfigById(metadataConfigId));
-        return metadataConfig;
+        try {
+            MetadataConfig metadataConfig = checkNotNull(metadataConfigService.findMetadataConfigById(metadataConfigId));
+            return  ResponseEntity.status(HttpStatus.FOUND)
+                    .body(metadataConfig);
+        } catch (MetadataException exp) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(exp.getMessage());
+        }
     }
 
     @PreAuthorize("#oauth2.hasScope('server')")
     @RequestMapping(value = "/metaconfig", method = RequestMethod.GET)
-    public List<MetadataConfig> getMetadataConfigs() {
-        List<MetadataConfig> metadataConfigs = checkNotNull(metadataConfigService.findAllMetadataConfig());
-        return metadataConfigs;
+    public ResponseEntity getMetadataConfigs() {
+        try {
+            List<MetadataConfig> metadataConfigs = checkNotNull(metadataConfigService.findAllMetadataConfig());
+            return ResponseEntity.status(HttpStatus.FOUND)
+            .body(metadataConfigs);
+        }catch (MetadataException exp){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(exp.getMessage());
+        }
     }
 
     @PreAuthorize("#oauth2.hasScope('server')")
     @RequestMapping(value = "/metaconfig/owner/{ownerId}", method = RequestMethod.GET)
-    public List<MetadataConfig> getMetadataConfigsByOwnerId(@PathVariable String ownerId) {
-        List<MetadataConfig> metadataConfigs = checkNotNull(metadataConfigService.findAllMetadataConfigByOwnerId(ownerId));
-        return metadataConfigs;
+    public ResponseEntity getMetadataConfigsByOwnerId(@PathVariable String ownerId) {
+        try {
+            List<MetadataConfig> metadataConfigs = checkNotNull(metadataConfigService.findAllMetadataConfigByOwnerId(ownerId));
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .body(metadataConfigs);
+        }catch (MetadataException exp) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(exp.getMessage());
+        }
     }
 
     @PreAuthorize("#oauth2.hasScope('server')")
@@ -78,7 +98,7 @@ public class MetadataConfigController {
 
     private <T> T checkNotNull(T reference) throws MetadataException {
         if (reference == null) {
-            throw new MetadataException("Requested item wasn't found!");
+            throw new MetadataException("{\"error\":\"Requested item wasn't found!\"}");
         }
         return reference;
     }
