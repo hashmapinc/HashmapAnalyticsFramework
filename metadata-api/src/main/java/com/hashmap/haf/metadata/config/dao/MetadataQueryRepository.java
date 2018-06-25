63d97148cf317805e35f006be9b5e5ba8e0a588a
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-06-25 12:24:18 +0530

    changing primary key name for query_config table

--- metadata-api/src/main/java/com/hashmap/haf/metadata/config/dao/MetadataQueryRepository.java
+++ metadata-api/src/main/java/com/hashmap/haf/metadata/config/dao/MetadataQueryRepository.java
@@ -2,13 +2,12 @@ package com.hashmap.haf.metadata.config.dao;
 
 import com.hashmap.haf.metadata.config.entity.MetadataQueryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MetadataQueryRepository extends JpaRepository<MetadataQueryEntity, String> {
+public interface QueryconfigRepository  extends JpaRepository<MetadataQueryEntity, String> {
 
-    List<MetadataQueryEntity> findByMetadataConfigId(@Param("metadataConfigId") String metadataConfigId);
+    List<MetadataQueryEntity> findByMetadataId(String metadataId);
 }
