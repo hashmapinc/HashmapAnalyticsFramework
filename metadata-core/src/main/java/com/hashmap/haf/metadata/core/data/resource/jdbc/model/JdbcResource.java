b8ca135620d67bb37a91ec07fabb3ea0030c453b
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-07-04 17:26:07 +0530

    adding changes in jsbc resource for push pull and test connection

--- metadata-core/src/main/java/com/hashmap/haf/metadata/core/data/resource/jdbc/model/JdbcResource.java
+++ metadata-core/src/main/java/com/hashmap/haf/metadata/core/data/resource/jdbc/model/JdbcResource.java
@@ -1,12 +1,21 @@
 package com.hashmap.haf.metadata.core.data.resource.jdbc.model;
 
 import com.hashmap.haf.metadata.core.data.resource.DataResource;
+import com.mysql.jdbc.CommunicationsException;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
+import lombok.extern.slf4j.Slf4j;
 
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 public class JdbcResource extends DataResource<JdbcResourceId> {
 
     private String dbUrl;
     private String username;
     private String password;
+    final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 
     public JdbcResource() {
         super();
@@ -47,13 +56,82 @@ public class JdbcResource extends DataResource<JdbcResourceId> {
     }
 
     @Override
-    public void push() {
-
+    public void push(Map payload) {
+        //TODO : Will be implemented later, when we have JDBC as Sink for Metadata
     }
 
     @Override
-    public void pull() {
+    public Map pull(String query) {
+        Map<String, Object> payload = new HashMap<>();
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = createConnection();
+            statement = connection.createStatement();
 
+            ResultSet rs = statement.executeQuery(query);
+
+            while(rs.next()){
+                payload.put(rs.getString(1), rs.getObject(2));
+            }
+        } catch (CommunicationsException e) {
+            log.info("CommunicationsException : [{}]", e.getMessage());
+        } catch (MySQLSyntaxErrorException e) {
+            log.info("MySQLSyntaxErrorException : [{}]", e.getMessage());
+        } catch (SQLException e) {
+            log.info("SQLException : [{}]", e.getMessage());
+        } catch (Exception e) {
+            log.info("Exection : [{}]", e.getMessage());
+        } finally {
+            try {
+                if(statement != null)
+                    statement.close();
+            } catch(SQLException se){
+                log.info("Exection : [{}]", se.getMessage());
+                se.printStackTrace();
+            }
+            try {
+                if(connection != null)
+                    connection.close();
+            }catch(SQLException se){
+                log.info("Exection : [{}]", se.getMessage());
+                se.printStackTrace();
+            }
+        }
+        return payload;
+    }
+
+    @Override
+    public boolean testConnection() {
+        Connection connection = null;
+        try {
+            connection = createConnection();
+            return true;
+        } catch (CommunicationsException e) {
+            log.info("CommunicationsException : [{}]", e.getMessage());
+        } catch (MySQLSyntaxErrorException e) {
+            log.info("MySQLSyntaxErrorException : [{}]", e.getMessage());
+        } catch (SQLException e) {
+            log.info("SQLException : [{}]", e.getMessage());
+        } catch (Exception e) {
+            log.info("Exection : [{}]", e.getMessage());
+        } finally {
+            try {
+                if(connection != null)
+                    connection.close();
+            }catch(SQLException se){
+                log.info("Exection : [{}]", se.getMessage());
+                se.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private Connection createConnection() throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        Class.forName(JDBC_DRIVER);
+        connection = DriverManager.getConnection(this.dbUrl, this.username, this.password);
+        return connection;
     }
 
     @Override
