5ea11bd684aa1242f8eb8ea79c7ef63763a6fcae
Abhishek Pal <koolabhipal@gmail.com>
2018-12-17 11:43:47 +0530

    frequency check logic

--- data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/FrequencyQualityCheck.scala
+++ data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/FrequencyQualityCheck.scala
@@ -1,12 +1,58 @@
 package com.hashmap.dataquality.qualitycheck
 
-import com.hashmap.dataquality.data.KafkaInboundMsg
+import java.util.Optional
+
+import com.hashmap.dataquality.data.{KafkaInboundMsg, TsKvData}
+import com.hashmap.dataquality.metadata.{MetadataFetchService, TagMetaData}
+import com.hashmap.dataquality.util.JsonUtil
+import com.hashmapinc.tempus.MqttConnector
+import javax.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.stereotype.Service
 
 @Service
 class FrequencyQualityCheck extends QualityCheck {
 
-  override def check(deviceId: String, payload: KafkaInboundMsg): Unit = {
+  private val log = LoggerFactory.getLogger(classOf[FrequencyQualityCheck])
 
+  @Autowired
+  private val metadataFetchService: MetadataFetchService = null
+
+  @Value("${tempus.mqtt-url}") private val MQTT_URL = ""
+
+  @Value("${tempus.gateway-access-token}") private val ACCESS_TOKEN = ""
+
+  private var mqttConnector: MqttConnector = _
+
+  @PostConstruct
+  def init(): Unit = {
+    mqttConnector = new MqttConnector(MQTT_URL, ACCESS_TOKEN)
+  }
+
+  override def check(deviceId: String, payload: KafkaInboundMsg): Unit = {
+    val tagsWithMissedFrequency = checkTagsFrequency(deviceId, payload.tagList.toList)
+    publish(payload.deviceName, tagsWithMissedFrequency)
+  }
+
+  private def publish(deviceName: String, tagsWithFrequencyMismatch: List[String]): Unit = {
+    mqttConnector.publish(JsonUtil.toJson(Map("frequencyMismatchElements" -> tagsWithFrequencyMismatch)),
+      Optional.of(System.currentTimeMillis()),
+      Optional.empty(), deviceName)
+  }
+
+  private def checkTagsFrequency(deviceId: String, tagList: List[TsKvData]): List[String] = {
+    val tagsMetadata = metadataFetchService.getMetadataForDevice(deviceId) match {
+      case Right(metadata) => metadata
+      case Left(_) => log.error(s"Missing metadata for $deviceId. Tag frequency quality check failed"); return List.empty
+    }
+    tagsMetadata.filter(metadata => checkTagFrequencyMismatch(metadata, tagList)).map(_.tag)
+  }
+
+  private def checkTagFrequencyMismatch(metadata: TagMetaData, tagList: List[TsKvData]): Boolean = {
+    val tagPresenceCount = tagList.map(_.tag).count(metadata.tag.equals(_))
+    val timestampRange = tagList.map(_.ts)
+    val expectedCount = ((timestampRange.max - timestampRange.min) / metadata.avgTagFrequency.toLong).floor
+    tagPresenceCount < expectedCount
   }
 }
--- data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/TagPresenceQualityCheck.scala
+++ data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/TagPresenceQualityCheck.scala
@@ -3,15 +3,18 @@ package com.hashmap.dataquality.qualitycheck
 import java.util.Optional
 
 import com.hashmap.dataquality.data.{KafkaInboundMsg, TsKvData}
-import com.hashmap.dataquality.metadata.{MetadataFetchService, TagMetaData}
+import com.hashmap.dataquality.metadata.MetadataFetchService
 import com.hashmap.dataquality.util.JsonUtil
 import com.hashmapinc.tempus.MqttConnector
 import javax.annotation.PostConstruct
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.stereotype.Service
 
 @Service
 class TagPresenceQualityCheck extends QualityCheck {
+
+  private val log = LoggerFactory.getLogger(classOf[TagPresenceQualityCheck])
 
   @Autowired
   private val metadataFetchService: MetadataFetchService = null
@@ -20,7 +23,7 @@ class TagPresenceQualityCheck extends QualityCheck {
 
   @Value("${tempus.gateway-access-token}") private val ACCESS_TOKEN = ""
 
-  private var mqttConnector: MqttConnector = null
+  private var mqttConnector: MqttConnector = _
 
   @PostConstruct
   def init(): Unit = {
@@ -28,7 +31,7 @@ class TagPresenceQualityCheck extends QualityCheck {
   }
 
   override def check(deviceId: String, payload: KafkaInboundMsg): Unit = {
-    val tagPresence: Map[String, Boolean] = checkTags(deviceId, payload.tagList.toList)
+    val tagPresence: Map[String, Boolean] = checkTagsPresence(deviceId, payload.tagList.toList)
     publish(payload.deviceName, tagPresence.filter(!_._2).keys.toList)
   }
 
@@ -38,10 +41,10 @@ class TagPresenceQualityCheck extends QualityCheck {
       Optional.empty(), deviceName)
   }
 
-  private def checkTags(deviceId: String, tagsPresent: List[TsKvData]): Map[String, Boolean] = {
+  private def checkTagsPresence(deviceId: String, tagsPresent: List[TsKvData]): Map[String, Boolean] = {
     val qualityAttributes = metadataFetchService.getMetadataForDevice(deviceId) match {
       case Right(metadata) => metadata
-      case Left(_) => return Map.empty //log
+      case Left(_) => log.error(s"Missing metadata for $deviceId. Tag presence quality check failed"); return Map.empty
     }
     qualityAttributes.map(mandatoryAttributes => (mandatoryAttributes.tag, tagsPresent.map(_.tag).contains(mandatoryAttributes.tag))).toMap
   }
