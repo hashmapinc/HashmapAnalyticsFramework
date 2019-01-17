e00d777cd43f1d7607953d9ca5bf37720dcf280c
Himanshu Mishra <hmdcisin@gmail.com>
2019-01-17 20:43:35 +0530

    Tempus-989 Refactored code.

--- data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/FrequencyQualityCheck.scala
+++ data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/FrequencyQualityCheck.scala
@@ -1,13 +1,9 @@
 package com.hashmap.dataquality.qualitycheck
 
-import java.util.Optional
-
-import com.hashmap.dataquality.data.{DeviceEvent, KafkaInboundMsg, TsKvData}
+import com.hashmap.dataquality.MqttPublisher
+import com.hashmap.dataquality.data.{KafkaInboundMsg, TsKvData}
 import com.hashmap.dataquality.metadata.{MetadataService, TagMetaData}
-import com.hashmap.dataquality.util.JsonUtil
 import com.hashmapinc.tempus.MqttConnector
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-import org.eclipse.paho.client.mqttv3.{MqttClient, MqttConnectOptions}
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.stereotype.Service
@@ -19,36 +15,14 @@ class FrequencyQualityCheck @Autowired()(metadataFetchService: MetadataService,
 
   private val log = LoggerFactory.getLogger(classOf[FrequencyQualityCheck])
 
-  private val DEVICE_EVENT_TOPIC = "v1/gateway/events"
   private val FREQUENCY_MISMATCH_ELEMENTS = "frequencyMismatchElements"
 
   override def check(deviceId: String, payload: KafkaInboundMsg): Unit = {
     val tagsWithMissedFrequency = tagsWithFrequencyMismatch(deviceId, payload.tagList.toList)
     if (tagsWithMissedFrequency.nonEmpty) {
-      publishAsTelemetry(payload.deviceName, tagsWithMissedFrequency)
-      publishAsEvents(payload.deviceName, tagsWithMissedFrequency)
+      MqttPublisher.publishAsTelemetry(payload.deviceName, tagsWithMissedFrequency, mqttConnector, FREQUENCY_MISMATCH_ELEMENTS)
+      MqttPublisher.publishAsEvents(payload.deviceName, tagsWithMissedFrequency, mqttConnector, FREQUENCY_MISMATCH_ELEMENTS)
     }
-  }
-
-  private def publishAsEvents(deviceName: String, tagsWithMissedFrequency: List[String]): Unit = {
-    try {
-      val client = new MqttClient(mqttConnector.getMqttUrl, MqttClient.generateClientId, new MemoryPersistence)
-      val options = new MqttConnectOptions
-      options.setUserName(mqttConnector.getAccessToken)
-      client.setTimeToWait(1000)
-      client.connect(options)
-      val event = DeviceEvent("data-quality-service", FREQUENCY_MISMATCH_ELEMENTS, JsonUtil.toJson(tagsWithMissedFrequency))
-      client.publish(DEVICE_EVENT_TOPIC, JsonUtil.toJson(Map(deviceName -> event)).getBytes(), 0, false)
-      client.close()
-    } catch {
-      case e: Exception => log.info("Exception " + e)
-    }
-  }
-
-  private def publishAsTelemetry(deviceName: String, tagsWithMissedFrequency: List[String]): Unit = {
-    mqttConnector.publish(JsonUtil.toJson(Map(FREQUENCY_MISMATCH_ELEMENTS -> tagsWithMissedFrequency)),
-      Optional.of(System.currentTimeMillis()),
-      Optional.empty(), deviceName)
   }
 
   private def tagsWithFrequencyMismatch(deviceId: String, tagList: List[TsKvData]): List[String] = {
--- data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/TagPresenceQualityCheck.scala
+++ data-quality-service/src/main/scala/com/hashmap/dataquality/qualitycheck/TagPresenceQualityCheck.scala
@@ -1,13 +1,10 @@
 package com.hashmap.dataquality.qualitycheck
 
-import java.util.Optional
-
-import com.hashmap.dataquality.data.{DeviceEvent, KafkaInboundMsg, TsKvData}
+import com.hashmap.dataquality.MqttPublisher
+import com.hashmap.dataquality.data.{KafkaInboundMsg, TsKvData}
 import com.hashmap.dataquality.metadata.MetadataService
 import com.hashmap.dataquality.util.JsonUtil
 import com.hashmapinc.tempus.MqttConnector
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-import org.eclipse.paho.client.mqttv3.{MqttClient, MqttConnectOptions}
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -18,37 +15,15 @@ class TagPresenceQualityCheck @Autowired()(metadataFetchService: MetadataService
 
   private val log = LoggerFactory.getLogger(classOf[TagPresenceQualityCheck])
 
-  private val DEVICE_EVENT_TOPIC = "v1/gateway/events"
   private val MISSING_ELEMENTS = "missingElements"
 
   override def check(deviceId: String, payload: KafkaInboundMsg): Unit = {
     log.info("Kafka Inbound Msg {}" + JsonUtil.toJson(payload))
     val tagsNotPresent = checkTagsPresence(deviceId, payload.tagList.toList)
     if (tagsNotPresent.nonEmpty) {
-      publishAsTelemetry(payload.deviceName, tagsNotPresent)
-      publishAsEvents(payload.deviceName, tagsNotPresent)
+      MqttPublisher.publishAsTelemetry(payload.deviceName, tagsNotPresent, mqttConnector, MISSING_ELEMENTS)
+      MqttPublisher.publishAsEvents(payload.deviceName, tagsNotPresent, mqttConnector, MISSING_ELEMENTS)
     }
-  }
-
-  private def publishAsEvents(deviceName: String, missingTags: List[String]): Unit = {
-    try {
-      val client = new MqttClient(mqttConnector.getMqttUrl, MqttClient.generateClientId, new MemoryPersistence)
-      val options = new MqttConnectOptions
-      options.setUserName(mqttConnector.getAccessToken)
-      client.setTimeToWait(1000)
-      client.connect(options)
-      val event = DeviceEvent("data-quality-service", MISSING_ELEMENTS, JsonUtil.toJson(missingTags))
-      client.publish(DEVICE_EVENT_TOPIC, JsonUtil.toJson(Map(deviceName -> event)).getBytes(), 0, false)
-      client.close()
-    } catch {
-      case e: Exception => log.info("Exception " + e)
-    }
-  }
-
-  private def publishAsTelemetry(deviceName: String, missingTags: List[String]): Unit = {
-    mqttConnector.publish(JsonUtil.toJson(Map(MISSING_ELEMENTS -> missingTags)),
-      Optional.of(System.currentTimeMillis()),
-      Optional.empty(), deviceName)
   }
 
   private def checkTagsPresence(deviceId: String, tagsPresent: List[TsKvData]): List[String] = {
