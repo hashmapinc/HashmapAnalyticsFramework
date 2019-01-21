1f8435c46366a8992a945e77383af0ca98dfb10e
Himanshu Mishra <hmdcisin@gmail.com>
2019-01-21 15:07:28 +0530

    Tempus-1027 Extracted some env variables to yml file.

--- data-quality-service/src/main/scala/com/hashmap/dataquality/config/MqttConnectorConfig.scala
+++ data-quality-service/src/main/scala/com/hashmap/dataquality/config/MqttConnectorConfig.scala
@@ -8,13 +8,17 @@ import org.springframework.stereotype.Component
 @Component
 class MqttConnectorConfig {
 
-  @Value("${tempus.mqtt-url}") private val MQTT_URL: String = ""
+  @Value("${tempus.mqtt-bind-address}") private val MQTT_BIND_ADDRESS: String = ""
+
+  @Value("${tempus.mqtt-bind-port}") private val MQTT_BIND_PORT: String = ""
 
   @Value("${tempus.gateway-access-token}") private val ACCESS_TOKEN: String = ""
 
+  private val MQTT_URL_FORMAT = "tcp://%s:%s"
+
   @Bean
   def mqttConnectorBean(): MqttConnector = {
-    new MqttConnector(MQTT_URL, ACCESS_TOKEN)
+    new MqttConnector(String.format(MQTT_URL_FORMAT, MQTT_BIND_ADDRESS, MQTT_BIND_PORT), ACCESS_TOKEN)
   }
 
 }
